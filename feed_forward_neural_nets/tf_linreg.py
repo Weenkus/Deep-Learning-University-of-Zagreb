b82ceb502ab64c53b79fc67843e926d5094f9152
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-10-22 22:57:04 +0200

    Finish task 3

--- feed_forward_neural_nets/tf_linreg.py
+++ feed_forward_neural_nets/tf_linreg.py
@@ -1,35 +1,62 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 
 def main():
-    ## 1. definicija računskog grafa
-    # podatci i parametri
-    X  = tf.placeholder(tf.float32, [None])
+    # Define the computation graph
+    X = tf.placeholder(tf.float32, [None])
     Y_ = tf.placeholder(tf.float32, [None])
     a = tf.Variable(0.0)
     b = tf.Variable(0.0)
 
-    # afini regresijski model
+    # Linear regression
     Y = a * X + b
 
-    # kvadratni gubitak
+    # Quadratic loss
     loss = (Y-Y_)**2
 
-    # optimizacijski postupak: gradijentni spust
-    trainer = tf.train.GradientDescentOptimizer(0.1)
-    train_op = trainer.minimize(loss)
+    # Gradients
+    grad_a = 2 * a * (a*X + b - Y_)
+    grad_b = 2 * (a*X + b - Y_)
 
-    ## 2. inicijalizacija parametara
+    # core_inv = tf.matrix_inverse(tf.matmul(X, X, transpose_a=True))
+    # pseudo_inverz = tf.matmul(core_inv, X, transpose_b=True)
+    # grad_w = tf.matmul(pseudo_inverz, Y_)
+
+    # Optimization via gradient descent
+    direct = False
+    trainer = tf.train.GradientDescentOptimizer(0.1)
+    if direct:
+        train_op = trainer.minimize(loss)
+    else:
+        grads_and_vars = trainer.compute_gradients(loss, [a, b])
+        train_op = trainer.apply_gradients(grads_and_vars)
+
+    # Initialise the TF parameters
     sess = tf.Session()
     sess.run(tf.initialize_all_variables())
 
-    ## 3. učenje
-    # neka igre počnu!
+    # Inputs
+    feed_dict={X: [1, 2], Y_: [3, 5]}
+
+    # Train the model
     for i in range(100):
-        val_loss, _, val_a,val_b = sess.run([loss, train_op, a,b], feed_dict={X: [1,2], Y_: [3,5]})
-        print(i,val_loss, val_a,val_b)
+        val_loss, _, val_a, val_b = sess.run([loss, train_op, a, b], feed_dict=feed_dict)
+        print(i, val_loss,  val_a, val_b)
+
+        grads = sess.run(grads_and_vars, feed_dict=feed_dict)
+        print('Grads[trainer]:', [g[0] for g in grads])
+
+        custom_grad_a, custom_grad_b = sess.run([grad_a, grad_b], feed_dict=feed_dict)
+        print('Gradient a:', custom_grad_a)
+        print('Gradient b:', custom_grad_b)
+
+        a = tf.Print(custom_grad_a, [custom_grad_a], message="Gradient of a: ")
+        b = tf.Print(custom_grad_b, [custom_grad_b], message="Gradient of b: ")
+        print()
+
 
 if __name__ == '__main__':
     main()
