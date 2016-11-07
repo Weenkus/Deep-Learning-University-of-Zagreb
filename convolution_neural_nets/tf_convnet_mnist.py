50810a9ca91587fbe77f5c751ed0ee154b3612e2
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-11-07 14:17:27 +0100

    First convnet arhitecture]

--- convolution_neural_nets/tf_convnet_mnist.py
+++ convolution_neural_nets/tf_convnet_mnist.py
@@ -5,8 +5,7 @@ import tensorflow.contrib.layers as layers
 
 from tensorflow.examples.tutorials.mnist import input_data
 
-
-tf.app.flags.DEFINE_string('data_dir',  '/tmp/data/', 'Directory for storing data')
+tf.app.flags.DEFINE_string('data_dir', '/tmp/data/', 'Directory for storing data')
 
 
 def main():
@@ -16,36 +15,23 @@ def main():
     feature_number = dataset.train.images.shape[1]
     class_num = dataset.train.labels.shape[1]
 
-    X = tf.placeholder(tf.float32, [None, feature_number])
-    y_ = tf.placeholder(tf.float32, [None, class_num])
-
-    nn = build_model(train_x, train_y, 10, False)
-
-    sess = tf.Session()
-    init = tf.initialize_all_variables()
-    sess.run(init)
-
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(train_y, y_))
-    optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
-
-    for iteration in range(10):
-        _, loss = sess.run([optimizer, loss], feed_dict={X: train_x, y_: train_y})
-        print('Iteration: {0}, loss: {1}'.format(iteration, loss))
+    conv_net = TFConvNet(feature_number, class_num, False)
+    conv_net.train(train_x, train_y, test_x, test_y)
 
 
 def create_dataset(dataset):
     np.random.seed(int(time.time() * 1e6) % 2 ** 31)
 
     train_x = dataset.train.images
-    train_x = train_x.reshape([-1, 1, 28, 28])
+    train_x = train_x.reshape([-1, 28, 28, 1])
     train_y = dataset.train.labels
 
     valid_x = dataset.validation.images
-    valid_x = valid_x.reshape([-1, 1, 28, 28])
+    valid_x = valid_x.reshape([-1, 28, 28, 1])
     valid_y = dataset.validation.labels
 
     test_x = dataset.test.images
-    test_x = test_x.reshape([-1, 1, 28, 28])
+    test_x = test_x.reshape([-1, 28, 28, 1])
     test_y = dataset.test.labels
 
     train_mean = train_x.mean()
@@ -56,42 +42,93 @@ def create_dataset(dataset):
     return train_x, train_y, valid_x, valid_y, test_x, test_y
 
 
-def build_model(inputs, labels, num_classes, is_training):
-    weight_decay = 1e-3
-    bn_params = {
-        # Decay for the moving averages.
-        'decay': 0.999,
-        'center': True,
-        'scale': True,
-        # epsilon to prevent 0s in variance.
-        'epsilon': 0.001,
-        # None to force the updates during train_op
-        'updates_collections': None,
-        'is_training': is_training
-    }
+class TFConvNet(object):
+    def __init__(self, feature_num, class_num, is_training):
+        self.weight_decay = 1e-3
+        self.bn_params = {
+            # Decay for the moving averages.
+            'decay': 0.999,
+            'center': True,
+            'scale': True,
+            # epsilon to prevent 0s in variance.
+            'epsilon': 0.001,
+            # None to force the updates during train_op
+            'updates_collections': None,
+            'is_training': is_training
+        }
 
-    with tf.contrib.framework.arg_scope(
-        [layers.convolution2d],
-        kernel_size=3, stride=1, padding='SAME', activation_fn=tf.nn.relu,
-        normalizer_fn=layers.batch_norm, normalizer_params=bn_params,
-        weights_initializer=layers.variance_scaling_initializer(),
-        weights_regularizer=layers.l2_regularizer(weight_decay)
-    ):
+        self.feature_num = feature_num
+        self.class_num = class_num
 
-        net = layers.convolution2d(inputs, num_outputs=16, kernel_size=5, scope='conv1')
-        net = layers.max_pool2d(net, scope='pool1')
-        net = layers.relu(net, scope='relu1')
+        self.X = tf.placeholder(tf.float32, [None, feature_num])
+        self.y_ = tf.placeholder(tf.float32, [None, class_num])
 
-        net = layers.convolution2d(net, num_outputs=32, kernel_size=5, scope='conv2')
-        #net = layers.flatten(net, scope='flatten1')
+        with tf.contrib.framework.arg_scope(
+                [layers.convolution2d],
+                kernel_size=3, stride=1, padding='SAME', activation_fn=tf.nn.relu,
+                normalizer_fn=layers.batch_norm, normalizer_params=self.bn_params,
+                weights_initializer=layers.variance_scaling_initializer(),
+                weights_regularizer=layers.l2_regularizer(self.weight_decay)
+        ):
+            self.X = tf.reshape(self.X, [-1, 28, 28, 1])
 
-        net = layers.fully_connected(net, scope='fc1')
-        net = layers.relu(net, scope='relu2')
+            net = layers.convolution2d(self.X, num_outputs=8, kernel_size=5, scope='conv1')
+            net = layers.max_pool2d(net, kernel_size=2, stride=2, scope='pool1')
+            net = layers.convolution2d(net, num_outputs=16, kernel_size=5, scope='conv2')
+            net = layers.flatten(net, [-1, 7 * 7 * 16])
+            net = layers.fully_connected(net, num_outputs=32, activation_fn=tf.nn.relu, scope='fc1')
+            net = layers.fully_connected(net, num_outputs=self.class_num, scope='fc2')
+            self.y = layers.softmax(net, scope='softmax')
 
-        net = layers.fully_connected(net, scope='fc2')
-        net = layers.softmax(net, scope='softmax')
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.y, self.y_))
+        self.optimizer = tf.train.AdamOptimizer(0.001).minimize(self.loss)
 
-        return net
+        pred = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
+        self.acc = tf.reduce_mean(tf.cast(pred, tf.float32))
+
+        self.sess = tf.Session()
+
+    def train(self, X_train, y_train, X_test, y_test, epohes=2000, batch_size=50):
+        print("Starting to train")
+        self.sess.run(tf.initialize_all_variables())
+
+        batch_start = 0
+        batch_end = batch_start + batch_size
+        for iteration in range(epohes):
+            _, loss, probs = self.sess.run(
+                [self.optimizer, self.loss, self.y],
+                feed_dict={self.X: X_train[batch_start:batch_end], self.y_: y_train[batch_start:batch_end]}
+            )
+
+            if iteration % 100 == 0:
+                acc = self.sess.run(
+                    self.acc,
+                    feed_dict={self.X: X_train[batch_start:batch_end], self.y_: y_train[batch_start:batch_end]}
+                )
+
+                print('Iteration: {}, loss: {:2.4}, train acc: {:.2%}'.format(iteration, loss, acc))
+
+            batch_start = batch_end
+            batch_end += batch_size
+
+            if batch_end > len(X_train):
+                batch_start = 0
+                batch_end = batch_start + batch_size
+                X_train, y_train = self.__shuffle(X_train, y_train)
+
+        print("Training ended")
+
+        acc = self.sess.run(
+            self.acc,
+            feed_dict={self.X: X_test, self.y_: y_test}
+        )
+
+        print("Test acc:", acc)
+
+    def __shuffle(self, a, b):
+        p = np.random.permutation(len(a))
+        return a[p], b[p]
+
 
 if __name__ == '__main__':
     main()
