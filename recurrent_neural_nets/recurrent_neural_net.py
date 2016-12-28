feb845c43352223cf76f8b3940727f5496f83b03
Vinko Kodzoman <vinko.kodzoman@yahoo.com>
2016-12-28 11:49:11 +0100

    RNN is runnable :)

--- recurrent_neural_nets/recurrent_neural_net.py
+++ recurrent_neural_nets/recurrent_neural_net.py
@@ -1,10 +1,11 @@
 import numpy as np
 import dataset
+import time
 
 
 class RNN(object):
 
-    def __init__(self, hidden_size, sequence_length, vocab_size, learning_rate):
+    def __init__(self, hidden_size, sequence_length, vocab_size, learning_rate, batch_size):
         self.hidden_size = hidden_size
         self.sequence_length = sequence_length
         self.vocab_size = vocab_size
@@ -12,10 +13,10 @@ class RNN(object):
 
         self.U = np.array(np.random.randn(vocab_size, hidden_size))
         self.W = np.array(np.random.randn(hidden_size, hidden_size))
-        self.b = np.ones((hidden_size, 1))
+        self.b = np.ones((hidden_size, batch_size))
 
         self.V = np.array(np.random.randn(hidden_size, vocab_size))
-        self.c = np.ones((vocab_size, 1))
+        self.c = np.ones((vocab_size, batch_size))
 
         # memory of past gradients - rolling sum of squares for Adagrad
         self.memory_U = np.zeros_like(self.U)
@@ -36,7 +37,7 @@ class RNN(object):
         dU, dW, db = self.__rnn_backward(dh, cache)
         self.__update(dU, dW, db, dV, dc)
 
-        return loss, h
+        return loss, h[-1]
 
     def __rnn_step_forward(self, x, h_prev, U, W, b):
         # A single time step forward of a recurrent neural network with a
@@ -53,6 +54,7 @@ class RNN(object):
         h_temp = np.tanh(np.dot(x, U) + np.dot(h_prev, W))
         h_current = np.add(h_temp, b.T)
 
+
         # return the new hidden state and a tuple of values needed for the backward step
         return h_current, cache
 
@@ -66,7 +68,8 @@ class RNN(object):
         # W - hidden to hidden projection matrix (hidden size x hidden size)
         # b - bias of shape (hidden size x 1)
 
-        cache = h = []
+        cache = []
+        h = []
         h_previous = h0
         for time_step in range(self.sequence_length):
             h_current, cache_current = self.__rnn_step_forward(x[:, time_step], h_previous, U, W, b)
@@ -87,10 +90,10 @@ class RNN(object):
 
         x, h_prev = cache
 
-        dW = grad_next * h_prev
-        dU = grad_next * x
-        db = grad_next
-        dh_prev = grad_next * self.W
+        dW = np.dot(grad_next.T, h_prev)
+        dU = np.dot(grad_next.T, x).T
+        db = grad_next.T
+        dh_prev = np.dot(grad_next, self.W)
 
         # compute and return gradients with respect to each parameter
         # HINT: you can use the chain rule to compute the derivative of the
@@ -107,7 +110,7 @@ class RNN(object):
         dW = 0
         db = 0
         for cache_step in reversed(cache):
-            grad_next = dh * (1 - cache[1]**2)
+            grad_next = dh * (1 - cache_step[1]**2)
             dh_prev_step, dU_step, dW_step, db_step = self.__rnn_step_backward(grad_next, cache_step)
 
             dU += dU_step
@@ -117,23 +120,27 @@ class RNN(object):
         # compute and return gradients with respect to each parameter
         # for the whole time series.
         # Why are we not computing the gradient with respect to inputs (x)?
-        for gradient in [dU, dW, db]:
-            np.clip(gradient, -self.grad_clip, self.grad_clip)
+        dU = np.clip(dU, -self.grad_clip, self.grad_clip)
+        dW = np.clip(dW, -self.grad_clip, self.grad_clip)
+        db = np.clip(db, -self.grad_clip, self.grad_clip)
 
         return dU, dW, db
 
     @staticmethod
     def __output(h, V, c):
         # Calculate the output probabilities of the network
-        return np.dot(h, V) + c.T
+        return np.add(np.dot(h, V), c.T)
 
     def __output_loss_and_grads(self, h, V, c, y):
         # Calculate the loss of the network for each of the outputs
 
         # h - hidden states of the network for each timestep.
-        #     the dimensionality of h is (batch size x sequence length x hidden size (the initial state is irrelevant for the output)
+        #     the dimensionality of h is
+        #     (batch size x sequence length x hidden size (the initial state is irrelevant for the output)
+        #
         # V - the output projection matrix of dimension hidden size x vocabulary size
         # c - the output bias of dimension vocabulary size x 1
+        #
         # y - the true class distribution - a one-hot vector of dimension
         #     vocabulary size x 1 - you need to do this conversion prior to
         #     passing the argument. A fast way to create a one-hot vector from
@@ -155,25 +162,28 @@ class RNN(object):
         dh = 0
         dV = 0
         dc = 0
-        for time_step, h_current in enumerate(reversed(h)):
+        for time_step, h_current in enumerate(h):
             output = self.__output(h_current, V, c)
             y_pred = self.softmax(output)
 
-            y_true = y[len(y) - time_step]
+            y_true = y[:, time_step, :]
 
             y_grad = y_pred - y_true
-            loss -= y_true * np.log(y_pred)
+            loss -= np.sum(y_true * np.log(y_pred))
 
-            dc += y_grad
-            dV += h_current * y_grad
-            dh += (V * y_pred) - y_true
+            dc += y_grad.T
+            dV += np.dot(h_current.T, y_grad)
+
+            # TODO FIX dh
+            dh += np.dot(V, y_pred.T).T
+            #dh += y_true
 
         return loss, dh, dV, dc
 
     @staticmethod
     def softmax(x):
         e_x = np.exp(x - np.max(x))
-        return e_x / e_x.sum(axis=0)
+        return (e_x + 1) / (e_x.sum(axis=0) + 2)
 
     def one_hot(self, vector):
         """
@@ -230,18 +240,18 @@ def run_language_model(max_epochs, hidden_size=100, sequence_length=30, learning
         hidden_size=hidden_size,
         sequence_length=sequence_length,
         vocab_size=vocab_size,
-        learning_rate=learning_rate)
+        learning_rate=learning_rate,
+        batch_size=batch_size)
 
     current_epoch = 0
     batch = 0
 
     h0 = np.zeros((batch_size, hidden_size))
 
-    average_loss = 0
-
     while current_epoch < max_epochs:
+        losses = []
         for e, x, y in parser.minibatch_generator():
-            if e:
+            if e == 0:
                 current_epoch += 1
                 h0 = np.zeros((batch_size, hidden_size))
                 # why do we reset the hidden state here?
@@ -256,17 +266,20 @@ def run_language_model(max_epochs, hidden_size=100, sequence_length=30, learning
             # of the unroll. You should then use that hidden state as the
             # input for the next minibatch. In this way, we artificially
             # preserve context between batches.
+
             loss, h0 = rnn.step(h0, x_oh, y_oh)
+            losses.append(loss)
 
             if batch % sample_every == 0:
                 # run sampling (2.2)
                 pass
             batch += 1
-            input()
+
+        print 'Epoch: {0}, loss: {1}'.format(current_epoch, np.average(losses))
 
 
 def main():
-    run_language_model(2)
+    run_language_model(max_epochs=1000, learning_rate=1e-1, hidden_size=100, sequence_length=30, batch_size=16)
 
 if __name__ == '__main__':
     main()
